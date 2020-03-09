df49cd14443a5644e4d4382df4d8cc297a54798e
AurélioCosta <arcosta@gmail.com>
2020-03-09 17:13:38 -0300

    Update servidores-unb.py
    Atualização do filtro do pandas

--- servidores-unb.py
+++ servidores-unb.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
 url_data="http://www.portaltransparencia.gov.br/download-de-dados/servidores/202001_Servidores"
-filtro_unb="Fundao Universidade de Braslia"
+filtro_unb="Fundação Universidade de Brasília"
 filtro_cargo="PROFESSOR DO MAGISTERIO SUPERIOR"
 
 print("Carregando servidores")
-iter_csv = pd.read_csv("./202001_Cadastro.csv", iterator=True, sep=';', header=0, encoding='latin-1')
+iter_csv = pd.read_csv("./202001_Cadastro.csv", sep=';', header=0, encoding='latin-1')
 
-servidores_unb = pd.concat([chunk[chunk['ORG_EXERCICIO'] == filtro_unb] for chunk in iter_csv])
-print(type(servidores_unb))
-#profs_unb = pd.concat([chunk[chunk['DESCRICAO_CARGO'] == filtro_cargo] for chunk in servidores_unb])
+servidores_unb = iter_csv[iter_csv['ORG_EXERCICIO'].str.contains(filtro_unb)]
 
-print("Carregados")
+profs_unb = servidores_unb[servidores_unb['DESCRICAO_CARGO'].str.contains(filtro_cargo)]
+
+
+print("Carregados {} professore de {} servidores".format(len(profs_unb), len(servidores_unb)))
